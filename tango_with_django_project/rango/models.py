8dde1b8fed27e145bd3102467298a9af24d3c48f
Declan McLaren <2888985m@student.gla.ac.uk>
2025-02-14 04:11:34 +0000

    user profile

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -7,7 +8,7 @@ class Category(models.Model):
     likes = models.IntegerField(default=0)
 
     slug = models.SlugField(unique=True)
-    
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
@@ -26,3 +27,10 @@ class Page(models.Model):
 	
     def __str__(self): 
 	    return self.title
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    def __str__(self):
+        return self.user.username
